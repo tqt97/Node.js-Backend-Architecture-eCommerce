d0617b1d4ec11f710aaf4181f57341192ef2a000
tqt97 <kutuanonline199@gmail.com>
2024-01-17 17:14:06 +0700

    add http status code

--- src/core/error.response.js
+++ src/core/error.response.js
@@ -1,24 +1,7 @@
 'use strict'
 
-const StatusCode = {
-    BAD_REQUEST: 400,
-    UNAUTHORIZED: 401,
-    FORBIDDEN: 403,
-    NOT_FOUND: 404,
-    CONFLICT: 409,
-    TOO_MANY_REQUESTS: 429,
-    INTERNAL_SERVER_ERROR: 500
-}
 
-const ReasonStatusCode = {
-    BAD_REQUEST: 'Bad Request',
-    UNAUTHORIZED: 'Unauthorized',
-    FORBIDDEN: 'Forbidden',
-    NOT_FOUND: 'Not Found',
-    CONFLICT: 'Conflict',
-    TOO_MANY_REQUESTS: 'Too Many Requests',
-    INTERNAL_SERVER_ERROR: 'Internal Server Error'
-}
+const {StatusCode, ReasonPhrases} = require('../utils/httpStatusCode.js')
 
 class ErrorResponse extends Error {
     constructor(message, status) {
@@ -28,15 +11,21 @@ class ErrorResponse extends Error {
 }
 
 class ConflictRequestError extends ErrorResponse {
-    constructor(message = ReasonStatusCode.CONFLICT, status = StatusCode.CONFLICT) {
+    constructor(message = ReasonPhrases.CONFLICT, status = StatusCode.CONFLICT) {
         super(message, status)
     }
 }
 
 class BadRequestError extends ErrorResponse {
-    constructor(message = ReasonStatusCode.BAD_REQUEST, status = StatusCode.BAD_REQUEST) {
+    constructor(message = ReasonPhrases.BAD_REQUEST, status = StatusCode.BAD_REQUEST) {
         super(message, status)
     }
 }
 
-module.exports = { ErrorResponse, ConflictRequestError, BadRequestError }
+class AuthFailureError extends ErrorResponse {
+    constructor(message = ReasonPhrases.UNAUTHORIZED, status = StatusCode.UNAUTHORIZED) {
+        super(message, status)
+    }
+}
+
+module.exports = { ConflictRequestError, BadRequestError, AuthFailureError }
